4180cd26663e4d21deb807be4baddec63b2c4e93
seoha-kim <chalcoalk@naver.com>
2020-10-13 22:05:56 +0900

    2020-10-13 updates

---
+++ JavaProgramming/src/chapter3/DoWhileSample.java
@@ -0,0 +1,14 @@
+package chapter3;
+
+public class DoWhileSample {
+
+	public static void main(String[] args) {
+		char a = 'a';
+		do {
+			System.out.print(a);
+			a = (char)(a+1);
+			} while (a <= 'z');
+
+	}
+
+}
